8b534d24ea1aeb16cd6aaed60a52b3d3a12d66bf
Justin49 <justin.bell@hotmail.fr>
2021-02-09 15:28:03 +0100

    Changement d'image de fond à chaque clique sur une vignette d'un jeu fait

---
+++ backgroundImageChange.js
@@ -0,0 +1,21 @@
+$('#sackboy').on('click', function() {
+    $('body').css('background-image', 'url(assets/background-img/bg-sackboy.png)');
+});
+$('#demons-souls').on('click', function() {
+    $('body').css('background-image', 'url(assets/background-img/bg-demons-souls.png)');
+});
+$('#dirt5').on('click', function() {
+    $('body').css('background-image', 'url(assets/background-img/bg-dirt-5.png)');
+});
+$('#spiderman').on('click', function() {
+    $('body').css('background-image', 'url(assets/background-img/bg-spider-man.png)');
+});
+$('#watch-dogs').on('click', function() {
+    $('body').css('background-image', 'url(assets/background-img/bg-watch-dogs.png)');
+});
+$('#XIII').on('click', function() {
+    $('body').css('background-image', 'url(assets/background-img/bg-XIII.png)');
+});
+$('#yakuza').on('click', function() {
+    $('body').css('background-image', 'url(assets/background-img/bg-yakuza.png)');
+});
